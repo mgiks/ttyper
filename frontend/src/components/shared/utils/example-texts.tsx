92dfe2869467eed1cd5ab1376baa68c62edb6f40
mgiks <lidamil6785@gmail.com>
2025-02-13 23:27:30 +0600

    feat: add example texts

--- frontend/src/components/shared/utils/example-texts.tsx
+++ frontend/src/components/shared/utils/example-texts.tsx
@@ -12,3 +12,38 @@ export const moonText = `
   way so it can be seen. The moon needs not fame or money to be powerful. The 
   moon never asks you to go to war to defend it. Be like the moon.” 
 `
+
+export const tomText = `
+  Tom clapped his hands and cried: "Tom, Tom! Your guests are tired, and you had 
+  near forgotten! Come now, my merry friends, and Tom will refresh you! You 
+  shall clean grimy hands, and wash your weary faces; cast off your muddy cloaks 
+  and comb out your tangles!" He opened the door, and they followed him down a 
+  short passage and round a sharp turn. They came to a low room with a sloping 
+  roof (a penthouse, it seemed, built on the north end of the house). Its walls 
+  were of clean stone, but they were mostly covered with green hanging mats and 
+  yellow curtains. The floor was flagged, and strewn with fresh green rushes. 
+  There were four deep mattresses, each piled with white blankets, laid on the 
+  floor along one side. Against the opposite wall was a long bench laden with 
+  wide earthenware basins, and beside it stood brown ewers filled with water, 
+  some cold, some steaming hot. There were soft green slippers set ready beside 
+  each bed.
+`
+
+export const foxText = `
+  The quick brown fox jumps over the lazy dog.
+`
+
+export const problemText = `
+  Ultimately, this is not about solving your problems. It's about realizing 
+  that there are no problems. Only situations - to be dealt with now, or to be 
+  left alone and accepted as part of the "isness" of the present moment until 
+  they change or can be dealt with.
+`
+
+export const corportionText = `
+  But what many people who have never formed a corporation don't know is that 
+  a corporation is not really a thing. A corporation is merely a file folder 
+  with some legal documents in it, sitting in some attorney's office and 
+  registered with a state government agency. It's not a big building or a 
+  factory or a group of people.
+`
