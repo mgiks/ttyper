a9d962f4e05ae05c4978f2aadfa9077096ea1745
mgiks <lidamil6785@gmail.com>
2025-03-10 00:51:48 +0600

    feat: database querying

--- backend/db/db.go
+++ backend/db/db.go
@@ -3,32 +3,51 @@ package db
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type database struct {
-	URL string
+	url     string
+	pool    *pgxpool.Pool
+	context context.Context
 }
 
-var Dbpool *pgxpool.Pool
-
 func ConnectToDB(ctx context.Context) *database {
-	db := database{URL: os.Getenv("DATABASE_URL")}
+	dbPass := os.Getenv("POSTGRES_PASSWORD")
+	dbPort := os.Getenv("POSTGRES_PORT")
+	dbName := os.Getenv("POSTGRES_DB")
 
-	Dbpool, err := pgxpool.New(ctx, db.URL)
+	dbUrl := fmt.Sprintf(
+		"postgres://postgres:%s@localhost:%s/%s",
+		dbPass,
+		dbPort,
+		dbName,
+	)
+
+	dbPool, err := pgxpool.New(ctx, dbUrl)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to connect to the database: %v\n", err)
-		os.Exit(1)
+		log.Fatalf("Unable to connect to the database: %v\n", err)
 	}
-	defer Dbpool.Close()
 
-	err = Dbpool.Ping(ctx)
+	db := database{url: dbUrl, pool: dbPool, context: ctx}
+
+	err = db.pool.Ping(ctx)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to ping the database: %v\n", err)
-		os.Exit(1)
+		log.Fatalf("Unable to ping the database: %v\n", err)
 	}
 
 	return &db
+}
+
+func (db *database) Query(query string, args ...any) pgx.Rows {
+	rows, err := db.pool.Query(db.context, query, args...)
+	if err != nil {
+		log.Fatalf("Query %v failed: %v\n", query, err)
+	}
+
+	return rows
 }
