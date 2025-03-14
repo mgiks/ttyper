c027ece34a7778fb3fe253867858fddbf22ec8f2
mgiks <lidamil6785@gmail.com>
2025-03-14 18:41:26 +0600

    feat: load environmental variables

--- backend/main.go
+++ backend/main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
+	"github.com/joho/godotenv"
 	"github.com/mgiks/ttyper/typing"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Failed to load environmental variables: %v\n", err)
+	}
+
 	tsMux := typing.NewTypingServer()
 
-	err := http.ListenAndServe(":8000", tsMux)
-
+	err = http.ListenAndServe(":8000", tsMux)
 	if err != nil {
 		panic(err)
 	}
