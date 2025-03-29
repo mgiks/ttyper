1892d413b1c4eb08b1e9ae75846289357c3c713a
mgiks <lidamil6785@gmail.com>
2025-03-29 13:57:02 +0600

    feat: add function for key checking

--- frontend/src/components/typing-container/utils/trackText.tsx
+++ frontend/src/components/typing-container/utils/trackText.tsx
@@ -1,15 +1,31 @@
 export function trackText(text: string) {
   const trackedText = text.split('')
   let currentCharIndex = 0
+  let wrongKeyIndex = -1
 
   return function (key: string) {
+    if (key === 'Backspace') {
+      currentCharIndex > 0 ? currentCharIndex -= 1 : wrongKeyIndex = -1
+      return
+    }
     const currentChar = trackedText[currentCharIndex]
-    const nextCharIndex = currentCharIndex + 1
-    currentCharIndex = nextCharIndex
+    const charIndex = currentCharIndex
+    currentCharIndex += 1
 
-    if (key != currentChar) {
+    if (currentChar === key && charIndex <= wrongKeyIndex) {
+      wrongKeyIndex = -1
+      return true
+    }
+
+    if (wrongKeyIndex !== -1) {
       return false
     }
+
+    if (key !== currentChar) {
+      wrongKeyIndex = charIndex
+      return false
+    }
+
     return true
   }
 }
