8f37a8d5b3ee3e608967cca7e4cc441c4fd49050
mgiks <lidamil6785@gmail.com>
2025-04-07 23:27:44 +0600

    refactor: move leading text to text area

--- frontend/src/components/typing-container/TextArea.tsx
+++ frontend/src/components/typing-container/TextArea.tsx
@@ -1,15 +1,11 @@
 import { useContext, useEffect, useRef } from 'react'
 import './TextArea.css'
 import { LeadingAndTralingTextContext } from './context/LeadingAndTralingTextContext'
-import LeadingText from './LeadingText'
+import { WrongTextStartIndexContext } from './context/WrongTextStartIndexContext'
 
 function TextArea() {
-  const { trailingText } = useContext(
-    LeadingAndTralingTextContext,
-  )
   const textAreaRef = useRef<HTMLDivElement>(null)
   const cursorRef = useRef<HTMLSpanElement>(null)
-
   useEffect(() => {
     const cursor = cursorRef.current
     const textArea = textAreaRef.current
@@ -28,9 +24,20 @@ function TextArea() {
     }
   })
 
+  const { leadingText } = useContext(LeadingAndTralingTextContext)
+  const { wrongTextStartIndex } = useContext(WrongTextStartIndexContext)
+  const rightText = wrongTextStartIndex > -1
+    ? leadingText.slice(0, wrongTextStartIndex)
+    : leadingText
+  const wrongText = wrongTextStartIndex > -1
+    ? leadingText.slice(wrongTextStartIndex)
+    : ''
+  const { trailingText } = useContext(LeadingAndTralingTextContext)
+
   return (
     <div id='text-area' ref={textAreaRef}>
-      <LeadingText />
+      <span id='right-text' className='unselectable-text'>{rightText}</span>
+      <span id='wrong-text' className='unselectable-text'>{wrongText}</span>
       <span id='cursor' ref={cursorRef}></span>
       <span id='trailing-text' className='unselectable-text'>
         {trailingText}
