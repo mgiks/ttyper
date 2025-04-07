3c1f12c646cff808ea5ab9bd5bee1b54f8393e4c
mgiks <lidamil6785@gmail.com>
2025-04-07 23:17:18 +0600

    feat: automatically scroll text going out of bounds

--- frontend/src/components/typing-container/TextArea.tsx
+++ frontend/src/components/typing-container/TextArea.tsx
@@ -7,10 +7,31 @@ function TextArea() {
   const { trailingText } = useContext(
     LeadingAndTralingTextContext,
   )
+  const textAreaRef = useRef<HTMLDivElement>(null)
+  const cursorRef = useRef<HTMLSpanElement>(null)
+
+  useEffect(() => {
+    const cursor = cursorRef.current
+    const textArea = textAreaRef.current
+    if (!cursor || !textArea) {
+      return
+    }
+    const cursorYPosition = cursor.getBoundingClientRect().y
+    const textAreaTopYCoor = textArea.getBoundingClientRect().y
+    const textAreaHeight = textArea.getBoundingClientRect().height
+    const textAreaBottomYCoor = textAreaTopYCoor + textAreaHeight
+    const isCursorInsideTextArea = textAreaTopYCoor < cursorYPosition &&
+      cursorYPosition < textAreaBottomYCoor
+
+    if (!isCursorInsideTextArea) {
+      cursor.scrollIntoView({ behavior: 'smooth' })
+    }
+  })
+
   return (
-    <div id='text-area'>
+    <div id='text-area' ref={textAreaRef}>
       <LeadingText />
-      <span id='cursor'></span>
+      <span id='cursor' ref={cursorRef}></span>
       <span id='trailing-text' className='unselectable-text'>
         {trailingText}
       </span>
